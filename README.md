ae9179507ea82464ba6a302a8e23e28deeb82a47
Ephrem Mekuria <ephremmekuria@gmail.com>
2021-09-26 15:13:37 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# wanza
+ WANZA mobile app UI for the company WANZA furnishing built in flutter
---
+++ android/app/src/main/kotlin/com/example/wanza/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wanza
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
